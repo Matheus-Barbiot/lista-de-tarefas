ba5645ededf53b490328020e9b82fbdaf0b7fa44
matheus-barbiot <matheusbarbiot01@gmail.com>
2025-11-27 23:25:32 -0300

    Estilos criados, scripts finalizados

--- script.js
+++ script.js
@@ -3,24 +3,26 @@ const botaoAdicionar = document.getElementById('adicionar');
 const listaTarefas = document.getElementById('lista');
 
 // Template tarefa
-const templateTarefa = `
-<input type="checkbox">
-<p>Tarefa Exemplo</p>
-<input type="button" value="x">
-`
+const templateTarefa = `<input type="checkbox">
+<p contenteditable="true">Clique para editar</p>
+<input type="button" value="X">`;
 
-// Array com as tarefas listadas
-let tarefas = [];
 
 // funcao para adicionar uma nova tarefa
 function addTarefa() {
     let novaTarefa = document.createElement('div');
     novaTarefa.classList.add('tarefa');
     novaTarefa.innerHTML = templateTarefa;
-    tarefas.push(novaTarefa);
-
+    botaoRemover = novaTarefa.children[2];
     listaTarefas.appendChild(novaTarefa);
+    botaoRemover.addEventListener('click',(event) => removerTarefa(event));
 }
 
+// Funcao para remover uma tarefa
+function removerTarefa(event) {
+    const botaoClicado = event.target;
+    botaoClicado.parentElement.remove();
+
+}
 // Evento de clique no botao adicionar
 botaoAdicionar.addEventListener('click', addTarefa);
